88d64d82eb9ebaebfb1577fab0b3c21f1cf45954
Picadio <sashabedryk2004@gmail.com>
2024-05-31 09:30:05 +0300

    add view books

---
+++ books/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from books import views
+
+app_name = 'books'
+
+urlpatterns = [
+    path('', views.books, name='books'),
+
+]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from books.models import Book, Author
+
+
+def books(request):
+    books = Book.objects.all()
+    context = {'title': 'Books', 'books': books}
+    return render(request, 'book_list.html', context)
+
+
